8e6886db101b035713194dc1857790f40aff5c1e
jarek <jaroslaw.malek@codete.com>
2017-02-01 09:47:08 +0100

    #1: change kernel.send method to publish

--- jupyter/Groovy/src/main/java/com/twosigma/beaker/jupyter/Comm.java
+++ jupyter/Groovy/src/main/java/com/twosigma/beaker/jupyter/Comm.java
@@ -149,7 +149,7 @@ public class Comm {
     map.put(COMM_ID, getCommId());
     map.put(DATA, data);
     message.setContent(map);
-    kernel.send(message); //TODO check if right ?
+    kernel.publish(message); //TODO check if right ?
   }
   
   public void handleMsg(Message parentMessage) throws NoSuchAlgorithmException{
