9a80fcf7e07622029b1fb5c65f4ac8b12fe61650
Konstantin Tarletski <konstantin.tarletski@ignite.ee>
2017-02-03 17:04:27 +0200

    issue #6 "get beaker object working in groovy" -- Beaker Value some implementation

--- jupyter/Groovy/src/main/java/com/twosigma/beaker/groovy/NamespaceClient.java
+++ jupyter/Groovy/src/main/java/com/twosigma/beaker/groovy/NamespaceClient.java
@@ -15,15 +15,39 @@
  */
 package com.twosigma.beaker.groovy;
 
+import java.io.Serializable;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.lappsgrid.jupyter.groovy.handler.IHandler;
+import org.lappsgrid.jupyter.groovy.msg.Message;
+
+import com.twosigma.beaker.jupyter.Comm;
+import com.twosigma.beaker.jupyter.CommNamesEnum;
+
 public class NamespaceClient {
+  
+  private class ObjectHolder<T>{
+    
+    private T value;
+
+    public T getValue() {
+      return value;
+    }
+
+    public void setValue(T value) {
+      this.value = value;
+    }
+    
+  }
   
   private static Map<String,NamespaceClient> nsClients = new ConcurrentHashMap<String,NamespaceClient>();
   private static String currentSession;
   
   private SimpleEvaluationObject currentCeo = null;
+  private Comm autotranslationComm = null;
   
   public SimpleEvaluationObject getOutputObj() {
     return currentCeo;
@@ -52,5 +76,60 @@ public class NamespaceClient {
     nsClients.remove(sessionId);
     currentSession = null;
   }
+  
+  public Object set(String name, Object value) {
+    try {
+      Comm c = getAutotranslationComm();
+      HashMap<String, Serializable> data = new HashMap<>();
+      data.put("name", name);
+      data.put("value", value.toString());
+      data.put("sync", true);
+      c.setData(data);
+      c.send();
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    }
+    return "set:" + name + "/" + value;
+  }
+
+  public Object setFast(String name, Object value) {
+    return "setFast:" + name + "/" +value;
+  }
+
+  public Object unset(String name) {
+    return "unset:" + name;
+  }
+
+  public Object get(final String name) {
+    final ObjectHolder ret = new ObjectHolder();
+    try {
+      Comm c = getAutotranslationComm();
+      HashMap<String, Serializable> data = new HashMap<>();
+      data.put("name", name);
+      //data.put("value", value.toString());
+      //data.put("sync", true);
+      c.setData(data);
+      c.addMsgCallbackList(new IHandler<Message>() {
+        
+        public void handle(Message message) throws NoSuchAlgorithmException{
+          ret.setValue(message.getContent());
+        }
+        
+      });
+      c.send();
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    }
+    return ret.getValue();
+  }
+
+  protected Comm getAutotranslationComm() throws NoSuchAlgorithmException{
+    if(autotranslationComm == null){
+      autotranslationComm = new Comm(CommNamesEnum.BEAKER_AUTOTRANSLATION);
+      autotranslationComm.open();
+    }
+    return autotranslationComm;
+  }
+  
   
 }
