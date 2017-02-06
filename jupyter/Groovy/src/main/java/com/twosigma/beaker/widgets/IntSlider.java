a5a5838f9c07abdbfc70e16d1ce1ac468aba2d78
jarek <jaroslaw.malek@codete.com>
2017-02-06 14:06:24 +0100

    #38: sync IntSlider properties

--- jupyter/Groovy/src/main/java/com/twosigma/beaker/widgets/IntSlider.java
+++ jupyter/Groovy/src/main/java/com/twosigma/beaker/widgets/IntSlider.java
@@ -26,7 +26,16 @@ import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class IntSlider implements Widget {
+public class IntSlider extends Widget {
+
+  public static final String DISABLED = "disabled";
+  public static final String VALUE = "value";
+  public static final String STEP = "step";
+  public static final String ORIENTATION = "orientation";
+  public static final String MAX = "max";
+  public static final String MIN = "min";
+  public static final String VISIBLE = "visible";
+  public static final String DESCRIPTION = "description";
 
   private String _view_name = "IntSliderView";
   private String _model_name = "IntSliderModel";
@@ -34,8 +43,15 @@ public class IntSlider implements Widget {
   private String _view_module = "jupyter-js-widgets";
 
   private Comm comm;
-  private int value;
   private Layout layout;
+  private Integer value = 0;
+  private Boolean disabled = false;
+  private Integer step = 1;
+  private String orientation = "horizontal";
+  private Integer max = 100;
+  private Integer min = 0;
+  private Boolean visible = true;
+  private String description="";
 
   public IntSlider() throws NoSuchAlgorithmException {
     comm = new Comm(Utils.uuid(), CommNamesEnum.JUPYTER_WIDGET);
@@ -52,9 +68,72 @@ public class IntSlider implements Widget {
     return value;
   }
 
-  public void setValue(int value) throws NoSuchAlgorithmException {
+  public void setValue(int value) {
     this.value = value;
-    sendValueUpdate(value);
+    sendUpdate(VALUE, value);
+  }
+
+  public boolean isDisabled() {
+    return disabled;
+  }
+
+  public void setDisabled(boolean disabled) {
+    this.disabled = disabled;
+    sendUpdate(DISABLED, disabled);
+  }
+
+  public Integer getStep() {
+    return step;
+  }
+
+  public void setStep(Integer step) {
+    this.step = step;
+    sendUpdate(STEP, step);
+  }
+
+  public String getOrientation() {
+    return orientation;
+  }
+
+  public void setOrientation(String orientation) {
+    this.orientation = orientation;
+    sendUpdate(ORIENTATION, orientation);
+  }
+
+  public Integer getMax() {
+    return max;
+  }
+
+  public void setMax(Integer max) {
+    this.max = max;
+    sendUpdate(MAX, max);
+  }
+
+  public Integer getMin() {
+    return min;
+  }
+
+  public void setMin(Integer min) {
+    this.min = min;
+    sendUpdate(MIN, min);
+  }
+
+  public Boolean getVisible() {
+    return visible;
+  }
+
+  public void setVisible(Boolean visible) {
+    this.visible = visible;
+    sendUpdate(VISIBLE, visible);
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    sendUpdate(DESCRIPTION, description);
+    this.description = description;
   }
 
   private void openComm(final Comm comm) throws NoSuchAlgorithmException {
@@ -69,7 +148,7 @@ public class IntSlider implements Widget {
       public void handle(Message message) throws NoSuchAlgorithmException {
         Map data = (Map) message.getContent().get("data");
         Map sync_data = (Map) data.get("sync_data");
-        int value = (int) sync_data.get("value");
+        int value = (int) sync_data.get(VALUE);
         updateValue(value);
       }
     });
@@ -86,36 +165,26 @@ public class IntSlider implements Widget {
     content.put("_view_module", _view_module);
     content.put("_view_name", _view_name);
     content.put("_range", false);
+    content.put("layout", "IPY_MODEL_" + layout.getComm().getCommId());
     content.put("background_color", null);
     content.put("continuous_update", true);
-    content.put("description", "");
-    content.put("disabled", false);
+    content.put(DESCRIPTION, this.description);
+    content.put(DISABLED, this.disabled);
     content.put("font_family", "");
     content.put("font_size", "");
     content.put("font_style", "");
     content.put("font_weight", "");
-    content.put("layout", "IPY_MODEL_"+layout.getComm().getCommId());
-    content.put("max", 100);
-    content.put("min", 0);
+    content.put(MAX, this.max);
+    content.put(MIN, this.min);
     content.put("msg_throttle", 3);
-    content.put("orientation", "horizontal");
+    content.put(ORIENTATION, orientation);
     content.put("readout", true);
     content.put("readout_format", "d");
     content.put("slider_color", null);
-    content.put("step", 1);
-    content.put("value", value);
-    content.put("visible", true);
+    content.put(STEP, 1);
+    content.put(VALUE, value);
+    content.put(VISIBLE, this.visible);
     return content;
-  }
-
-  private void sendValueUpdate(int value) throws NoSuchAlgorithmException {
-    HashMap<String, Serializable> content = new HashMap<>();
-    content.put("method", "update");
-    HashMap<Object, Object> state = new HashMap<>();
-    state.put("value", value);
-    content.put("state", state);
-    getComm().setData(content);
-    getComm().send();
   }
 
 }
