3330e5a78ef4eefec86cb1e8252770e273db86cb
jarek <jaroslaw.malek@codete.com>
2017-02-07 12:43:13 +0100

    #37: add Dropdown widget and example

---
+++ jupyter/Groovy/src/main/java/com/twosigma/beaker/widgets/Dropdown.java
@@ -0,0 +1,169 @@
+/*
+ *  Copyright 2017 TWO SIGMA OPEN SOURCE, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.twosigma.beaker.widgets;
+
+import com.twosigma.beaker.jupyter.Comm;
+import com.twosigma.beaker.jupyter.CommNamesEnum;
+import com.twosigma.beaker.jupyter.Utils;
+import org.lappsgrid.jupyter.groovy.handler.IHandler;
+import org.lappsgrid.jupyter.groovy.msg.Message;
+
+import java.io.Serializable;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.twosigma.beaker.widgets.Layout.IPY_MODEL;
+import static com.twosigma.beaker.widgets.Layout.LAYOUT;
+
+public class Dropdown extends Widget {
+
+  protected static final String VALUE = "value";
+  protected static final String VISIBLE = "visible";
+  protected static final String DESCRIPTION = "description";
+  protected static final String MSG_THROTTLE = "msg_throttle";
+  protected static final String DISABLED = "disabled";
+  public static final String OPTIONS_LABELS = "_options_labels";
+
+  private final String _view_name = "DropdownView";
+  private final String _model_name = "DropdownModel";
+  private final String _model_module = "jupyter-js-widgets";
+  private final String _view_module = "jupyter-js-widgets";
+
+  private Comm comm;
+  private Layout layout;
+  private String value = "";
+
+  private Boolean disabled = false;
+  private String description = "";
+  private Integer msg_throttle = 3;
+  private Boolean visible = true;
+  private String[] options = new String[0];
+
+  public Dropdown() throws NoSuchAlgorithmException {
+    comm = new Comm(Utils.uuid(), CommNamesEnum.JUPYTER_WIDGET);
+    layout = new Layout();
+    openComm(comm);
+  }
+
+  private void openComm(final Comm comm) throws NoSuchAlgorithmException {
+    comm.setData(content());
+    addValueChangeMsgCallback(comm);
+    comm.open();
+  }
+
+  private void addValueChangeMsgCallback(final Comm comm) {
+    comm.addMsgCallbackList(new IHandler<Message>() {
+      @Override
+      public void handle(Message message) throws NoSuchAlgorithmException {
+        Map data = (Map) message.getContent().get("data");
+        Map sync_data = (Map) data.get("sync_data");
+        String value = (String) sync_data.get(VALUE);
+        updateValue(value);
+      }
+    });
+  }
+
+  private void updateValue(String value) {
+    this.value = value;
+  }
+
+  @Override
+  public Comm getComm() {
+    return this.comm;
+  }
+
+  private HashMap<String, Serializable> content() {
+    HashMap<String, Serializable> content = new HashMap<>();
+    content.put("_model_module", _model_module);
+    content.put("_model_name", _model_name);
+    content.put("_view_module", _view_module);
+    content.put("_view_name", _view_name);
+    content.put(LAYOUT, IPY_MODEL + layout.getComm().getCommId());
+    content.put(VALUE, this.value);
+
+    content.put(DESCRIPTION, this.description);
+    content.put(DISABLED, this.disabled);
+    content.put(VISIBLE, this.visible);
+    content.put(MSG_THROTTLE, this.msg_throttle);
+
+    content.put("background_color", null);
+    content.put("font_family", "");
+    content.put("font_size", "");
+    content.put("font_style", "");
+    content.put("font_weight", "");
+
+    content.put("color", null);
+    content.put("button_style", "");
+    content.put(OPTIONS_LABELS, this.options);
+
+    return content;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+    sendUpdate(VALUE, value);
+  }
+
+  public Boolean getDisabled() {
+    return disabled;
+  }
+
+  public void setDisabled(Boolean disabled) {
+    this.disabled = disabled;
+    sendUpdate(DISABLED, disabled);
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+    sendUpdate(DESCRIPTION, description);
+  }
+
+  public Integer getMsg_throttle() {
+    return msg_throttle;
+  }
+
+  public void setMsg_throttle(Integer msg_throttle) {
+    this.msg_throttle = msg_throttle;
+    sendUpdate(MSG_THROTTLE, msg_throttle);
+  }
+
+  public Boolean getVisible() {
+    return visible;
+  }
+
+  public void setVisible(Boolean visible) {
+    this.visible = visible;
+    sendUpdate(VISIBLE, visible);
+  }
+
+  public String[] getOptions() {
+    return options;
+  }
+
+  public void setOptions(String[] options) {
+    this.options = options;
+    sendUpdate(OPTIONS_LABELS, options);
+  }
+}
