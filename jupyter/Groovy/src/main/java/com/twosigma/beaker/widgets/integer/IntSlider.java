07aceb287b78fdbfb4b434563bf9b2ed202f57e0
jarek <jaroslaw.malek@codete.com>
2017-02-08 14:56:29 +0100

    #37: move int widgets to package

--- jupyter/Groovy/src/main/java/com/twosigma/beaker/widgets/integer/IntSlider.java
+++ jupyter/Groovy/src/main/java/com/twosigma/beaker/widgets/integer/IntSlider.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package com.twosigma.beaker.widgets;
+package com.twosigma.beaker.widgets.integer;
 
 import java.io.Serializable;
 import java.security.NoSuchAlgorithmException;
