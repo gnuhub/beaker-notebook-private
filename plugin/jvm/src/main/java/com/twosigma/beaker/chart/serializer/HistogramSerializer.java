9e308ebe4941c6dd72c7829df492792469860725
jarek <jaroslaw.malek@codete.com>
2017-02-14 11:13:53 +0100

    #49: add comm functionality to histogram

--- plugin/jvm/src/main/java/com/twosigma/beaker/chart/serializer/HistogramSerializer.java
+++ plugin/jvm/src/main/java/com/twosigma/beaker/chart/serializer/HistogramSerializer.java
@@ -29,6 +29,12 @@ public class HistogramSerializer extends AbstractChartSerializer<Histogram> {
   public static final String GRAPHICS_LIST = "graphics_list";
   public static final String BIN_COUNT = "bin_count";
   public static final String COLOR = "color";
+  public static final String COLORS = "colors";
+  public static final String NAMES = "names";
+  public static final String DISPLAY_MODE = "displayMode";
+  public static final String CUMULATIVE = "cumulative";
+  public static final String NORMED = "normed";
+  public static final String LOG = "log";
 
   @Override
   public void serialize(Histogram histogram, JsonGenerator jgen, SerializerProvider provider) throws
@@ -39,7 +45,7 @@ public class HistogramSerializer extends AbstractChartSerializer<Histogram> {
     serialize(histogram, jgen);
 
     if (histogram.getColors() != null) {
-      jgen.writeObjectField("colors", histogram.getColors());
+      jgen.writeObjectField(COLORS, histogram.getColors());
     } else {
       jgen.writeObjectField(COLOR, histogram.getColor());
     }
@@ -56,11 +62,11 @@ public class HistogramSerializer extends AbstractChartSerializer<Histogram> {
     if (histogram.getRangeMax() != null)
       jgen.writeObjectField("range_max", histogram.getRangeMax());
     jgen.writeObjectField(BIN_COUNT, histogram.getBinCount());
-    jgen.writeObjectField("cumulative", histogram.getCumulative());
-    jgen.writeObjectField("normed", histogram.getNormed());
-    jgen.writeObjectField("log", histogram.getLog());
-    jgen.writeObjectField("displayMode", histogram.getDisplayMode());
-    jgen.writeObjectField("names", histogram.getNames());
+    jgen.writeObjectField(CUMULATIVE, histogram.getCumulative());
+    jgen.writeObjectField(NORMED, histogram.getNormed());
+    jgen.writeObjectField(LOG, histogram.getLog());
+    jgen.writeObjectField(DISPLAY_MODE, histogram.getDisplayMode());
+    jgen.writeObjectField(NAMES, histogram.getNames());
 
     jgen.writeEndObject();
 
