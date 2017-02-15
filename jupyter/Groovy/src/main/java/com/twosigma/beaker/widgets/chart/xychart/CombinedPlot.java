2e3b3a4e927f72898f045b18da4644c2fef78735
jarek <jaroslaw.malek@codete.com>
2017-02-15 14:06:41 +0100

    #49: add comm functionality to CombinedPlot

---
+++ jupyter/Groovy/src/main/java/com/twosigma/beaker/widgets/chart/xychart/CombinedPlot.java
@@ -0,0 +1,67 @@
+/*
+ *  Copyright 2017 TWO SIGMA OPEN SOURCE, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.twosigma.beaker.widgets.chart.xychart;
+
+import com.twosigma.beaker.chart.serializer.CombinedPlotSerializer;
+import com.twosigma.beaker.jupyter.Comm;
+import com.twosigma.beaker.widgets.CommFunctionality;
+import com.twosigma.beaker.widgets.chart.InternalPlot;
+import com.twosigma.beaker.widgets.internal.InternalWidget;
+import com.twosigma.beaker.widgets.internal.InternalWidgetContent;
+import com.twosigma.beaker.widgets.internal.InternalWidgetUtils;
+import com.twosigma.beaker.widgets.internal.SerializeToString;
+
+import java.io.Serializable;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+
+public class CombinedPlot extends com.twosigma.beaker.chart.xychart.CombinedPlot implements CommFunctionality, InternalWidget, InternalPlot {
+
+  private Comm comm;
+
+  public CombinedPlot() throws NoSuchAlgorithmException {
+    this.comm = InternalWidgetUtils.createComm(this, new InternalWidgetContent() {
+      @Override
+      public void addContent(HashMap<String, Serializable> content) {
+        content.put(InternalWidgetUtils.MODEL_NAME, MODEL_NAME_VALUE);
+        content.put(InternalWidgetUtils.VIEW_NAME, VIEW_NAME_VALUE);
+      }
+    });
+  }
+
+  @Override
+  public Comm getComm() {
+    return this.comm;
+  }
+
+  @Override
+  public com.twosigma.beaker.chart.xychart.CombinedPlot setXLabel(String xLabel) {
+    com.twosigma.beaker.chart.xychart.CombinedPlot combinedPlot = super.setXLabel(xLabel);
+    sendUpdate(CombinedPlotSerializer.X_LABEL, SerializeToString.toJson(this));
+    return combinedPlot;
+  }
+
+  @Override
+  public com.twosigma.beaker.chart.xychart.CombinedPlot setxLabel(String xLabel) {
+    return setXLabel(xLabel);
+  }
+
+  private void sendUpdate(final String propertyName, final Object value) {
+    if (this.comm != null) {
+      this.comm.sendUpdate(propertyName, value);
+    }
+  }
+}
