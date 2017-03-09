752343e9e55d8a806cf9f9b447cb6a5c4ded03f2
Scott Draves <spot@draves.org>
2017-03-09 11:20:29 -0500

    remove import *

--- jupyter/kernel/base/src/main/java/com/twosigma/beaker/table/TableDisplay.java
+++ jupyter/kernel/base/src/main/java/com/twosigma/beaker/table/TableDisplay.java
@@ -16,7 +16,13 @@
 package com.twosigma.beaker.table;
 
 import java.io.Serializable;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
 
