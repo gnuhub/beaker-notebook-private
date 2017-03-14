f54317ab7d8a817c62665ef94c03000b0e283438
Konstantin Tarletski <konstantin.tarletski@ignite.ee>
2017-03-14 15:04:52 +0200

    issue #103 "create new kernel for scala"

--- kernel/scala/src/main/java/com/twosigma/beaker/scala/ScalaDefaultVariables.java
+++ kernel/scala/src/main/java/com/twosigma/beaker/scala/ScalaDefaultVariables.java
@@ -27,41 +27,25 @@ public class ScalaDefaultVariables {
    */
   //TODO add to kernel this classes, then uncomment
   public static final String[] IMPORTS ={
-      "static java.lang.Math.*",
-      //"graxxia.*",
-      //"com.twosigma.beaker.NamespaceClient",
-      //"com.twosigma.beaker.BeakerProgressUpdate",
+      "com.twosigma.beaker.NamespaceClient",
+      "com.twosigma.beaker.BeakerProgressUpdate",
       "com.twosigma.beaker.chart.Color",
-      "com.twosigma.beaker.chart.xychart.*",
-      "com.twosigma.beaker.chart.xychart.plotitem.*",
+      "com.twosigma.beaker.chart.GradientColor",
       "com.twosigma.beaker.chart.legend.*",
       "com.twosigma.beaker.chart.Filter",
-      //"com.twosigma.beaker.easyform.*",
-      //"com.twosigma.beaker.easyform.formitem.*",
-      "com.twosigma.beaker.chart.GradientColor",
+      "com.twosigma.beaker.chart.xychart.*",
+      "com.twosigma.beaker.chart.xychart.plotitem.*",
       "com.twosigma.beaker.chart.categoryplot.*",
       "com.twosigma.beaker.chart.categoryplot.plotitem.*",
-      "com.twosigma.beaker.chart.histogram.*",
       //"com.twosigma.beaker.chart.treemap.*",
       //"com.twosigma.beaker.chart.treemap.util.*",
       //"net.sf.jtreemap.swing.*",
-      "com.twosigma.beaker.chart.heatmap.HeatMap",
-      //"com.twosigma.beaker.jvm.object.*",
-      "com.twosigma.beaker.chart.KeyboardCodes",
-      "java.util.concurrent.TimeUnit",
-      "com.github.lwhite1.tablesaw.api.*",
-      "com.github.lwhite1.tablesaw.columns.*",
-      "com.github.lwhite1.tablesaw.api.ml.clustering.*",
-      "com.github.lwhite1.tablesaw.reducing.*",
-      "com.github.lwhite1.tablesaw.api.ml.regression.*",
-      "static com.github.lwhite1.tablesaw.api.QueryHelper.*",
-      "com.github.lwhite1.tablesaw.filtering.*"//,
-      //"com.twosigma.beaker.table.*",
-      //"com.twosigma.beaker.table.format.*",
-      //"com.twosigma.beaker.table.renderer.*",
-      //"com.twosigma.beaker.table.highlight.*";
+      "com.twosigma.beaker.chart.histogram.*",
+      "com.twosigma.beaker.chart.heatmap.HeatMap"
+      //"com.twosigma.beaker.easyform.*",
+      //"com.twosigma.beaker.easyform.formitem.*",
       };
-
+ 
   /**
    * Default class path
    */
